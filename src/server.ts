7b52affc2376acf790e78bc0742dee429d110b9a
masonaimhigh <mvlogs054@gmail.com>
2024-06-04 03:38:26 +0900

    Type script

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,66 @@
+// TypeScript
+// Compiled languages: Java, GoLang, C, C++, C#, Rust => Compiling & Running,
+
+// Interpreted Languages: NodeJs, Python, PHP, Ruby   =>Running
+//Type script dynamic type integration ni olib kirgan, hosil qib bergan
+// typeScript = Java script + type script elements and components
+//Error type
+// Types in JavaScript
+// prmitive - string, number, boolean,null, undefined, symbol
+// object - objects, arrays, maps
+
+// let box: string;
+// box = "hello";
+// box = 100; // error type string
+
+// let box: string = "hello";
+// box = "100";
+
+// const counter: number = 100;
+// let stage: number | string = "hello";
+// stage = 100;
+
+// let pending: boolean;
+// pending = true;
+
+// const pending: boolean = true;
+
+//Object
+// interface
+// interface Person {
+//     name: string,
+//     age: number,
+//     nation: string
+// }
+// const person: Person = {
+//     name: "Martin",
+//     age: 300,
+//     nation: "Australian"
+// }
+
+// ARRAY
+// let skills: string[];
+// skills = ['Problem solving', "software design", 'Programming'];
+
+// let skill: (number | string)[];
+// skill = ['Problem solving', "software design", 'Programming', 100];
+
+//class
+
+// class Person {
+//   age: number;
+//   firstName: string;
+//   lastName: string;
+
+// constructor(age: number, firstName: string, lastName: string) {
+//   this.age = age;
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   }
+// }
+
+// const person1 = new Person(30, 'Martin', 'Robertson')
+
 // console.log("Executed");
 
 // import moment from 'moment';
@@ -6,3 +69,9 @@
 
 // const person: string = 'Martin';
 // const count: number = 100;
+
+//Architectual pattern: MVC, Dependency Injection, MVP - bakend suyagi, malumotlar oqimini tartibga soladigan vosita
+// MVC Model view controller
+
+//Design pattern: Midleeware, Decorator, - butun bakend tizimini tartibga soluvchi patternlar - malum bir bo'lim qo'l
+//Types: Creational Structural Bahavioral 
