1bf655a1baa9c83c038a12ccc360076bb19f5650
masonaimhigh <mvlogs054@gmail.com>
2024-06-04 20:36:24 +0900

    G task added

--- src/server.ts
+++ src/server.ts
@@ -75,3 +75,27 @@
 
 //Design pattern: Midleeware, Decorator, - butun bakend tizimini tartibga soluvchi patternlar - malum bir bo'lim qo'l
 //Types: Creational Structural Bahavioral 
+
+// G-TASK:
+
+// Shunday function tuzingki unga integerlardan iborat array pass bolsin va function
+//  bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+// MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+
+function getHighestIndex(arr) {
+    if (Array.isArray(arr)) {
+      let max = arr[0];
+      let maxIndex = 0;
+      for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+          max = arr[i];
+          maxIndex = i;
+        }
+      }
+      return maxIndex;
+    } else {
+      return "please insert array";
+    }
+  }
+  console.log(getHighestIndex([5, 41, 55, 99, 7]));
+  console.log(getHighestIndex([6, 41, 85, 39, 71]));
