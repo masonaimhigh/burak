42300f0d23f485cad6e67a540c343892eb52f276
masonaimhigh <mvlogs054@gmail.com>
2024-06-06 20:16:03 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+/** 1-ENTRANCE **/
+const app = express();
+// console.log("__dirname:",__dirname);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+/** 2-SESSIONS **/
+
+/** 3-VIEWS **/
+app.set('view', path.join(__dirname, 'views'));
+app.set('view engine', "ejsnp")
+/** 4-ROUTERS **/
+export default app // module.exports = app 
