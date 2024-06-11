fdc1d406d697727fbc5eb5f3a89c8159a5f7ed72
masonaimhigh <mvlogs054@gmail.com>
2024-06-11 17:31:15 +0900

    Revert "H2 task added"
    This reverts commit 1531dd606f9366eaf3ddfa7e2ee0075ec149ca23.

--- src/train.ts
+++ src/train.ts
@@ -1,21 +1,14 @@
-//H2-TASK: 
+// H-TASK: 
 
-// Shunday function tuzing, unga string argument pass bolsin. Function ushbu 
-// agrumentdagi digitlarni yangi stringda return qilsin
-// MASALAN: 
-function getDigits(input: string): string {
-    let result = '';
-    for (let i = 0; i < input.length; i++) {
-        const char = input[i];
-        if (char >= '0' && char <= '9') {
-            result += char;
-        }
-    }
-    return result;
+// shunday function tuzing, u integerlardan iborat arrayni argument sifatida 
+// qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+function positiveNumbersToStrings(numbers: number[]): string[] {
+    return numbers.filter(num => num > 0).map(num => num.toString());
 }
 
-
-const result = getDigits("m14i1t");
-console.log(result);  
-
+// Example usage
+console.log(positiveNumbersToStrings([1, -2, 3, 0, 4])); 
+console.log(positiveNumbersToStrings([-1, -2, -3]));    
+console.log(positiveNumbersToStrings([0, 5, 10]));      
 
