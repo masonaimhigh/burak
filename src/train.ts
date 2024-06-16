672ef687cd6f5850a65d477625a22133309483fd
masonaimhigh <mvlogs054@gmail.com>
2024-06-16 17:36:06 +0900

    feat: Task I added

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,36 @@
+/*
+I-TASK:
+
+Shunday function yozing, u parametridagi array ichida eng kop takrorlangan
+ raqamni topib qaytarsin.
+MASALAN: majorityElement([1,2,3,4,5,4,3,4]) return 4
+*/
+
+function majorityElement(nums: number[]): number {
+  const counts: { [key: number]: number } = {};
+  let majorityElement = nums[0];
+  let maxCount = 0;
+
+  for (const num of nums) {
+      if (counts[num] === undefined) {
+          counts[num] = 1;
+      } else {
+          counts[num]++;
+      }
+
+      if (counts[num] > maxCount) {
+          maxCount = counts[num];
+          majorityElement = num;
+      }
+  }
+
+  return majorityElement;
+}
+
+
+const result = majorityElement([1, 2, 3, 5, 5, 5, 3, 4]);
+console.log(result); 
+
 /* Project Standards: 
    - logging standards
    - naming standards
@@ -20,12 +53,12 @@
 // shunday function tuzing, u integerlardan iborat arrayni argument sifatida 
 // qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
 // MASALAN: getPositive([1, -4, 2]) return qiladi "12"
-function positiveNumbersToStrings(numbers: number[]): string[] {
-    return numbers.filter(num => num > 0).map(num => num.toString());
-}
+// function positiveNumbersToStrings(numbers: number[]): string[] {
+//     return numbers.filter(num => num > 0).map(num => num.toString());
+// }
 
-// Example usage
-console.log(positiveNumbersToStrings([1, -2, 3, 0, 4])); 
-console.log(positiveNumbersToStrings([-1, -2, -3]));    
-console.log(positiveNumbersToStrings([0, 5, 10]));      
+// // Example usage
+// console.log(positiveNumbersToStrings([1, -2, 3, 0, 4])); 
+// console.log(positiveNumbersToStrings([-1, -2, -3]));    
+// console.log(positiveNumbersToStrings([0, 5, 10]));      
 
