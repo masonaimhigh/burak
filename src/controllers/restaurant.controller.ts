7493d4c044add206dfcedff758d61061223cf355
masonaimhigh <mvlogs054@gmail.com>
2024-06-20 18:15:24 +0900

    feat: create checkAuthSession api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,6 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/member.service";  // M 
 import { AdminRequest, LoginInput, MemberInput } from '../libs/types/member';
 import { MemberType } from '../libs/enums/member.enum';
+import { Message } from '../libs/Errors';
 
 const memberService = new MemberService();
 
@@ -71,6 +72,20 @@ restaurantController.processLogin =  async (req: AdminRequest, res: Response) =>
         });
     } catch(err) {
         console.log('Error, processLogin:', err);
+        res.send(err);
+    }  
+}
+
+restaurantController.checkAuthSession =  async (req: AdminRequest, res: Response) => {
+    try {
+        console.log('checkAuthSession');
+        if (req.session?.member) 
+        res.send(`<script> alert ("${req.session.member.memberNick}")</script> `);
+        else res.send(`<script> alert ("${Message.NOT_AUTHENTICATED}")</script>`)
+        console.log("body:", req.body);
+        
+    } catch(err) {
+        console.log('Error, checkAuthSession:', err);
         res.send(err);
     }  
 }
