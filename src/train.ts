095fbb0a73fbc7fc637979d92003459ac011bf56
masonaimhigh <mvlogs054@gmail.com>
2024-07-07 17:52:04 +0900

    Q-TASK added

--- src/train.ts
+++ src/train.ts
@@ -1,39 +1,24 @@
 /*
-P-TASK:
-
-Shunday function yozing, u object qabul qilsin va arrayni object 
-arrayga otkazib arrayni qaytarsin qaytarsin.
-MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
-*/
-function objectToArray(obj: { [key: string]: any }): [string, any][] {
-  return Object.entries(obj);
-}
-
-
-const input = { a: 10, b: 20 };
-const result = objectToArray(input);
-console.log(result);
-
-/*
 Q-TASK:
 
-Shunday function yozing, u 2 ta parametrgga ega bolib birinchisi object,
- ikkinchisi string. Agar string parametr objectni propertysi bolsa true bolmasa false qaytarsin.
+Shunday function yozing, u 2 ta parametrgga ega bolib birinchisi object, 
+ikkinchisi string. Agar string parametr objectni propertysi bolsa true 
+bolmasa false qaytarsin.
 MASALAN: hasProperty({name: "BMW", model: "M3"}, "model") return true; 
 hasProperty({name: "BMW", model: "M3"}, "year") return false
 */
 
-/*
+function hasProperty(obj: object, prop: string): boolean {
 
-function hasProperty(obj: { [key: string]: any }, prop: string): boolean {
-  return obj.hasOwnProperty(prop);
+  const keys = Object.keys(obj);
+
+  return keys.includes(prop);
 }
 
-// Example 
-console.log(hasProperty({ name: "BMW", model: "M3" }, "model")); // true
-console.log(hasProperty({ name: "BMW", model: "M3" }, "year"));  // false
+// Example:
+console.log(hasProperty({name: "BMW", model: "M3"}, "model")); 
+console.log(hasProperty({name: "BMW", model: "M3"}, "year"));  
 
-*/
 
 
 
