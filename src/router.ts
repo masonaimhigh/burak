8703db1372ec2e16fdc916fe7e09b319365d5147
masonaimhigh <mvlogs054@gmail.com>
2024-07-21 17:17:13 +0900

    fix: modify getMember detail business logic

--- src/router.ts
+++ src/router.ts
@@ -7,7 +7,7 @@ router.post('/member/login', memberController.login);
 router.post("/member/signup", memberController.signup);
 router.post("/member/logout", memberController.verifyAuth, memberController.logout);
 
-router.get("/member/detail", memberController.verifyAuth);
+router.get("/member/detail", memberController.verifyAuth, memberController.getMemberDetail);
 
 // Product 
 
