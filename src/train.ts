fa303595054516ab889a8ec0e4dba42d1b4de92a
masonaimhigh <mvlogs054@gmail.com>
2024-07-21 17:26:18 +0900

    W-TASK added

--- src/train.ts
+++ src/train.ts
@@ -1,61 +1,35 @@
 /*
 
-T-TASK:
+W-TASK:
 
-Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va 
-ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
-MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
+Shunday function yozing, uni array va number parametrlari bolsin.
+Function arrayni numberda berilgan uzunlikda kesib bolaklarga ajratilgan 
+array holatida qaytarsin
+MASALAN: chunkArray([1,2,3,4,5,6,7,8,9,10], 3) return [[1,2,3], [4,5,6], [7,8,9], [10]]
 
 */
-/*
-function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
 
-  if (arr1.length === 0) return arr2;
-  if (arr2.length === 0) return arr1;
-
-  const result: number[] = [];
-  let i = 0, j = 0;
-
-  while (i < arr1.length && j < arr2.length) {
-      if (arr1[i] < arr2[j]) {
-          result.push(arr1[i]);
-          i++;
-      } else {
-          result.push(arr2[j]);
-          j++;
-      }
+function chunkArray<T>(array: T[], size: number): T[][] {
+  if (size <= 0) {
+      throw new Error("Kiritilgan son 0 dan katta bolishi kerak");
   }
-
-  return result.concat(arr1.slice(i)).concat(arr2.slice(j));
+  
+  const result: T[][] = [];
+  
+  for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
+  }
+  
+  return result;
 }
 
-const array1 = [0, 3, 4, 31, 90];
-const array2 = [4, 6, 30];
-const result = mergeSortedArrays(array1, array2);
-console.log(result); 
-*/
 
-// U-TASK:
+const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const chunkSize = 4;
+const chunkedArray = chunkArray(inputArray, chunkSize);
 
-// Shunday function yozing, uni number parametri bolsin va 0 dan berilgan 
-// parametrgacha bolgan oraliqdagi faqat toq sonlar nechtaligini return qilsin
-// MASALAN: sumOdds(9) return 4; sumOdds(11) return 5;
+console.log(chunkedArray); 
 
-function sumOdds(n: number): number {
-  let oddCount = 0;
-
-  for (let i = 0; i < n; i++) {
-      if (i % 2 !== 0) {
-          oddCount++;
-      }
-  }
-
-  return oddCount;
-}
-
-console.log(sumOdds(9));
-console.log(sumOdds(11)); 
-console.log(sumOdds(13)); 
 
 
 
