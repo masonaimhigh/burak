ba45e7d85fed9913961014be4b56e9c215efc2e0
masonaimhigh <mvlogs054@gmail.com>
2024-07-21 14:48:41 +0900

    feat: develop token creation via jwt

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -3,8 +3,10 @@ import { T } from "../libs/types/common";
 import MemberService from '../models/member.service';
 import { LoginInput, Member, MemberInput } from '../libs/types/member';
 import Errors from '../libs/Errors';
+import AuthService from '../models/Auth.service';
 
 const memberService = new MemberService();
+const authService = new AuthService();
 
 //REACT
 const memberController: T = {};
@@ -13,8 +15,9 @@ memberController.signup =  async (req: Request, res: Response) => {
         console.log('signup');
         const input: MemberInput = req.body,
          result:Member = await memberService.signup(input);
-
-        //TODO: TOKEN
+         const token = await authService.createToken(result);
+         console.log("token:",token);
+      
         res.json({member: result });
     } catch(err) {
         console.log('Error, signup:', err);
@@ -28,8 +31,10 @@ memberController.login =  async (req: Request, res: Response) => {
     try {
         console.log('login');
         const input: LoginInput = req.body,
-          result = await memberService.login(input);
-        //TODO: TOKEN
+          result = await memberService.login(input),
+          token = await authService.createToken(result);
+            console.log("token",token);
+        
 
         res.json({member: result });
     } catch(err) {
