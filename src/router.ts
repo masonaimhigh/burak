8aeb4ea7637ccc0d2b2fc49c722933f425b707b8
masonaimhigh <mvlogs054@gmail.com>
2024-07-25 17:29:47 +0900

    feat: develop getProduct rest Api

--- src/router.ts
+++ src/router.ts
@@ -27,6 +27,10 @@ router.get("/member/top-users", memberController.getTopUsers);
 // Product 
 router.get("/product/all", productController.getProducts)
 // router.get("/product/all/:id/:key", productController.getProducts)
+router.get("/product/:id",
+    memberController.retrieveAuth, 
+    productController.getProduct
+);
 // Order 
 
 
